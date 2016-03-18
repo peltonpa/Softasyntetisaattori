b75e3c57741be8073c677787bfa26fd2fec3eee6
Paavo Peltonen <peltonpa@dhcp-asv-98.helsinki.fi>
2016-03-18 18:56:01 +0200

    Projektiin lisätty main-classi

---
+++ Softasyntetisaattori/src/main/java/ohjelmoinnin/harkka/syntetisaattori/softasyntetisaattori/Main.java
@@ -0,0 +1,9 @@
+package ohjelmoinnin.harkka.syntetisaattori.softasyntetisaattori;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //toistaiseksi tyhjää
+    }
+    
+}
